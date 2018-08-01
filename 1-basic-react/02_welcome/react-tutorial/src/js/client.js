3502dc11fb3c47e2970af149c8c5bd11920f2ca3
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2018-08-02 05:01:07 +0900

    Added start script for dynamic loading

---
+++ 1-basic-react/02_welcome/react-tutorial/src/js/client.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Layout extends React.Component {
+  render() {
+    return (
+      <h1>Welcome!</h1>
+    );
+  }
+}
+
+const app = document.getElementById('app');
+ReactDOM.render(<Layout/>, app);
+
