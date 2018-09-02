3489237829f847321f39ce0ca8ce380536d7cc45
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2018-09-02 09:46:33 +0900

    Move directories

--- 5-redux-react/00_start_point/tweet-app/src/js/client.js
+++ 5-redux-react/00_start_point/tweet-app/src/js/client.js
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 
 import Layout from "./components/Layout";
+import store from "./store";
 
 const app = document.getElementById('app');
 
-ReactDOM.render(<Layout />, app);
+ReactDOM.render(
+  <Provider store={store}>
+    <Layout />
+  </Provider>, app);
--- 5-redux-react/tweet-app/src/js/components/Layout.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export default class Layout extends React.Component {
-  render() {
-    return null;
-  }
-}
-
