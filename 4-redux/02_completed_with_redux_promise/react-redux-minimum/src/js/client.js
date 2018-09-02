3796b291bc1874d283eddb449af8db0b0538ccdd
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2018-09-02 10:13:08 +0900

    Added resources of Redux with redux-promise

---
+++ 4-redux/02_completed_with_redux_promise/react-redux-minimum/src/js/client.js
@@ -0,0 +1,36 @@
+import { applyMiddleware, createStore } from "redux";
+import axios from "axios";
+import { createLogger } from "redux-logger";
+import promise from "redux-promise-middleware";
+
+const initialState = {
+  fetching: false,
+  fetched: false,
+  users: [],
+  error: null
+};
+
+const reducer = (state=initialState, action) => {
+  switch (action.type) {
+    case "FETCH_USERS_PENDING":
+      return {...state, fetching: true};
+    case "FETCH_USERS_REJECTED":
+      return {...state, fetching :false, error: action.payload};
+    case "FETCH_USERS_FULFILLED":
+      return {
+        ...state,
+        fetching: false,
+        fetched: true,
+        users: action.payload
+      };
+  }
+  return state;
+};
+
+const middleware = applyMiddleware(promise(), createLogger());
+const store = createStore(reducer, middleware);
+
+store.dispatch({
+  type: "FETCH_USERS",
+  payload: axios.get("http://localhost:18080")
+});
