23546110b381388f1b3f91eaf9e9f492db0272a3
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2018-09-17 22:25:59 +0900

    Added new mobx start point

---
+++ 6-mobx-react/00_start_point/react-mobx/src/js/client.js
@@ -0,0 +1,9 @@
+import "../css/index.css";
+import React from "react";
+import ReactDOM from "react-dom";
+
+import TodoList from "./TodoList";
+
+const app = document.getElementById("app");
+
+ReactDOM.render(<TodoList />, app);
---
+++ 6-mobx-react/00_start_point/react-mobx/webpack.config.js
@@ -0,0 +1,47 @@
+var debug   = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+var path    = require('path');
+
+module.exports = {
+  context: path.join(__dirname, "src"),
+  /* devtool: debug ? "inline-sourcemap" : false, */
+  entry: "./js/client.js",
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      exclude: /(node_modules|bower_components)/,
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          plugins: [
+            'react-html-attrs',
+            [require('@babel/plugin-proposal-decorators'), {legacy: true}]
+          ],
+          presets: ['@babel/preset-react', '@babel/preset-env']
+        }
+      }]
+    },
+    {
+      test: /\.css$/,
+      loader: "style-loader!css-loader"
+    }]
+  },
+  output: {
+    path: __dirname + "/src/",
+    filename: "client.min.js",
+    publicPath: '/'
+  },
+  devServer: {
+    historyApiFallback: true
+  },
+  plugins: debug ? [] : [
+    /* Search for equal or similar files and deduplicate them in the output. */
+    // new webpack.optimize.DedupePlugin(), /* It has been removed since Webpack 2 */
+    /* Assign the module nad chunk ids by occurrence count.
+       Ids that are used often get lower (shorter) ids.
+       This make ids predictable reduces total file size and is recommended. */
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    /* UglifyJS Webpack Plugin */
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ],
+};
