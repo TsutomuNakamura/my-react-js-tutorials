17607c1fc942236e3c12e86c4f546460fb83be16
TsutomuNakamura <tsuna.0x00@gmail.com>
2019-05-02 23:47:48 +0900

    Added update_packages.sh

---
+++ update_packages.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+main() {
+    local package_json
+    declare -a directories=()
+    local target
+
+    while read package_json; do
+        echo "--> $package_json"
+        directories+=($(dirname $package_json))
+    done < <(find . -mindepth 4 -maxdepth 4 -name package.json)
+
+    for target in "${directories[@]}"; do
+        echo "@ Updating $target @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+        pushd "$target"
+        npm-check-update
+        npm-check-updates -u
+        rm -f package-lock.json
+        npm install
+        popd
+    done
+
+    for target in "${directories[@]}"; do
+        if [[ "$target" == "./1-basic-react/00_start_point/react-tutorial" ]]; then
+            continue
+        fi
+        echo "# Testing $target ###########################################"
+        pushd "$target"
+        npm start
+        popd
+    done
+
+
+}
+
+main "$@"
+
