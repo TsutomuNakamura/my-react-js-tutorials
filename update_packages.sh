0f09f81a0ccd759ff79c37e379ba1f6fc84466db
TsutomuNakamura <tsuna.0x00@gmail.com>
2019-05-04 01:00:31 +0900

    Added update script

--- update_packages.sh
+++ update_packages.sh
@@ -16,21 +16,34 @@ main() {
         npm-check-update
         npm-check-updates -u
         rm -f package-lock.json
+        popd
+    done
+
+    for target in "${directories[@]}"; do
+        pushd "$target"
         npm install
         popd
     done
 
+    local test_server_pid=
     for target in "${directories[@]}"; do
         if [[ "$target" == "./1-basic-react/00_start_point/react-tutorial" ]]; then
             continue
         fi
         echo "# Testing $target ###########################################"
         pushd "$target"
+        if [[ -f ./test_server.js ]]; then
+            echo "Running test_server.js..."
+            node ./test_server.js &
+            test_server_pid=$!
+        fi
         npm start
+        if [[ -f ./test_server.js ]]; then
+            kill $test_server_pid
+            sleep 0.2
+        fi
         popd
     done
-
-
 }
 
 main "$@"
