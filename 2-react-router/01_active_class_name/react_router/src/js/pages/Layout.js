67bf810a5ee3389c3f84bb26287ebfafcbf6efe2
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2020-06-07 10:37:14 +0900

    Updated packages

--- 2-react-router/01_active_class_name/react_router/src/js/pages/Layout.js
+++ 2-react-router/01_active_class_name/react_router/src/js/pages/Layout.js
@@ -11,11 +11,11 @@ class Layout extends React.Component {
       <div>
         <h1>KillerNews.net</h1>
         {this.props.children}
-        <Link to="/archives/some-other-articles?date=yesterday&filter=none" class="btn btn-warning">archives (some other articles)</Link>
-        <Link to="/archives?date=today&filter=hot" class="btn btn-danger">archives</Link>
-        <NavLink to="/settings/main" class="btn btn-success" activeClassName="btn-danger">settings</NavLink>
-        <Link to="/settings/extra" class="btn btn-success">settings (extra)</Link>
-        <button class="btn btn-info" onClick={this.navigate.bind(this)}>feathred</button>
+        <Link to="/archives/some-other-articles?date=yesterday&filter=none" className="btn btn-warning">archives (some other articles)</Link>
+        <Link to="/archives?date=today&filter=hot" className="btn btn-danger">archives</Link>
+        <NavLink to="/settings/main" className="btn btn-success" activeClassName="btn-danger">settings</NavLink>
+        <Link to="/settings/extra" className="btn btn-success">settings (extra)</Link>
+        <button className="btn btn-info" onClick={this.navigate.bind(this)}>feathred</button>
       </div>
     );
   }
