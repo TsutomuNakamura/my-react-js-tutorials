f4c6c849937fbca4d142263ea131a168d6e155f7
kazuma yabuki <kazuma.yabuki@access-company.com>
2021-09-04 01:17:47 +0900

    4-redux storeにActionを送信してstateを増やしたり減らしたりする

--- 4-redux/00_start_point/react-redux-minimum/src/js/client.js
+++ 4-redux/00_start_point/react-redux-minimum/src/js/client.js
@@ -4,9 +4,11 @@ import { createStore } from "redux";
 //     console.log("reducer has been called.");
 // }
 const reducer = (state = 0, action) => { //3.送信されたActionのタイプから分岐処理
-    switch(action.type) {
+    switch (action.type) {
         case "INC":
             return state + 1;
+        case "DEC":
+            return state - 1;
     }
     return state;
 }
@@ -17,4 +19,8 @@ store.subscribe(() => {
     console.log("store changed", store.getState());//4.Actionが送信されたら通知
 });
 
-store.dispatch({type: "INC"});//2.storeにActionを送信する
+store.dispatch({ type: "INC" });//2.storeにActionを送信する
+store.dispatch({ type: "INC" });
+store.dispatch({ type: "INC" });
+store.dispatch({ type: "INC" });
+store.dispatch({ type: "DEC" });
