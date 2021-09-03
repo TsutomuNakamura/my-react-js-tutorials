e6e8146aaf45221219d5c15af1b2c9a2084542f3
kazuma yabuki <kazuma.yabuki@access-company.com>
2021-09-04 01:09:46 +0900

    4-redux dispatchメソッドとActionを追加して分岐処理

--- 4-redux/00_start_point/react-redux-minimum/src/js/client.js
+++ 4-redux/00_start_point/react-redux-minimum/src/js/client.js
@@ -1,1 +1,20 @@
-// import { createStore } from "redux";
+import { createStore } from "redux";
+
+// const reducer = () => {
+//     console.log("reducer has been called.");
+// }
+const reducer = (state = 0, action) => { //3.送信されたActionのタイプから分岐処理
+    switch(action.type) {
+        case "INC":
+            return state + 1;
+    }
+    return state;
+}
+
+const store = createStore(reducer, 1); //1.初期化される
+
+store.subscribe(() => {
+    console.log("store changed", store.getState());//4.Actionが送信されたら通知
+});
+
+store.dispatch({type: "INC"});//2.storeにActionを送信する
