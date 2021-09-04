e766ba2918494fdec2dc734b0956511a8242e82b
kazuma yabuki <kazuma.yabuki@access-company.com>
2021-09-05 01:53:59 +0900

    4-redux redux-promise を使ってみる

--- 4-redux/00_start_point/react-redux-minimum/src/js/client.js
+++ 4-redux/00_start_point/react-redux-minimum/src/js/client.js
@@ -1,7 +1,7 @@
 import { applyMiddleware, createStore } from "redux";
 import axios from "axios";
 import { createLogger } from "redux-logger";
-import thunk from "redux-thunk";
+import promise from "redux-promise-middleware";
 
 const initialState = {
     fetching: false,
@@ -12,30 +12,26 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case "FETCH_USERS_START":
+        case "FETCH_USERS_PENDING":
             return { ...state, fetching: true };
-        case "RECEIVE_USERS":
+        case "FETCH_USERS_FULFILLED":
             return {
                 ...state,
                 fetching: false,
                 fetched: true,
                 users: action.payload
             };
-        case "FETCH_USERS_ERROR":
+        case "FETCH_USERS_REJECTED":
             return { ...state, error: action.payload };
     }
     return state;
 };
 
-const middleware = applyMiddleware(thunk, createLogger());
+const middleware = applyMiddleware(promise, createLogger());
 
 const store = createStore(reducer, middleware);
 
-store.dispatch((dispatch) => {
-    dispatch({ type: "FETCH_USERS_START" });
-    axios.get("http://localhost:18080").then((response) => {
-        dispatch({ type: "RECEIVE_USERS", payload: response.data });
-    }).catch((err) => {
-        dispatch({ type: "FETCH_USERS_ERROR", payload: err });
-    });
+store.dispatch({
+    type: "FETCH_USERS",
+    payload: axios.get("http://localhost:18080")
 });
