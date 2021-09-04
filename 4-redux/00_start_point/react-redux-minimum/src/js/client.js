26f0be5f3e5a36a14a07ba073e1b88d8ef7a350c
kazuma yabuki <kazuma.yabuki@access-company.com>
2021-09-04 21:53:03 +0900

    4-redux 複数のReducer

--- 4-redux/00_start_point/react-redux-minimum/src/js/client.js
+++ 4-redux/00_start_point/react-redux-minimum/src/js/client.js
@@ -1,26 +1,37 @@
-import { createStore } from "redux";
+import { combineReducers, createStore } from "redux";
 
-// const reducer = () => {
-//     console.log("reducer has been called.");
-// }
-const reducer = (state = 0, action) => { //3.送信されたActionのタイプから分岐処理
-    switch (action.type) {
-        case "INC":
-            return state + action.payload;
-        case "DEC":
-            return state - action.payload;
+const userReducer = (state = {}, action) => {
+    switch(action.type) {
+        case "CHANGE_NAME":
+            state = {...state, name: action.payload }
+            break;
+        case "CHANGE_AGE":
+            state = {...state, age: action.payload }
+            break;
+    }
+    return state;
+}
+const tweetsReducer = (state = [], action) => {
+    switch(action.type){
+        case "ADD_TWEET":
+            state = state.concat({id: Date.now(), tweets: action.payload });
     }
     return state;
 }
 
-const store = createStore(reducer, 1); //1.初期化される
+const reducers = combineReducers({
+    user: userReducer,
+    tweets: tweetsReducer
+})
+
+const store = createStore(reducers); //1.初期化される
 
 store.subscribe(() => {
     console.log("store changed", store.getState());//4.Actionが送信されたら通知
 });
 
-store.dispatch({ type: "INC", payload: 1});//2.storeにActionを送信する
-store.dispatch({ type: "INC", payload: 2});
-store.dispatch({ type: "INC", payload: 22});
-store.dispatch({ type: "INC", payload: 222});
-store.dispatch({ type: "DEC", payload: 1000});
+store.dispatch({ type: "CHANGE_NAME", payload: "Tsutomu" });
+store.dispatch({ type: "CHANGE_AGE", payload: 35 });
+store.dispatch({ type: "CHANGE_AGE", payload: 36 });
+store.dispatch({ type: "ADD_TWEET", payload: "OMG LIKE LOL"})
+store.dispatch({ type: "ADD_TWEET", payload: "I am so like seriously like totally like right now"})
