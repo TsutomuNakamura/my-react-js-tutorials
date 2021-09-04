e2f7711b5a2efeecefc5661b27eb9a90764ab0d2
kazuma yabuki <kazuma.yabuki@access-company.com>
2021-09-05 01:05:23 +0900

    4-redux 非同期アプリケーション

--- 4-redux/00_start_point/react-redux-minimum/src/js/client.js
+++ 4-redux/00_start_point/react-redux-minimum/src/js/client.js
@@ -1,43 +1,16 @@
 import { applyMiddleware, createStore } from "redux";
+import { createLogger } from "redux-logger";
 
-const reducer = (state = 0, action) => {
-    switch (action.type) {
-        case "INC":
-            state = state + 1;
-            break;
-        case "DEC":
-            state = state - 1;
-            break;
-        case "ERR":
-            throw new Error("It's error!!!!");
-    }
+const reducer = (state={}, action) => {
     return state;
-}
+};
 
-const logger = (store) => (next) => (action) => {
-    console.log("action fired", action);
-    next(action);
-}
+const middleware = applyMiddleware(createLogger());
 
-const error = (store) => (next) => (action) => {
-    try {
-        next(action);
-    } catch (e) {
-        console.log("Error was occured", e);
-    }
+const store = createStore(reducer, middleware);
 
-}
-
-const middleware = applyMiddleware(logger, error);
-
-const store = createStore(reducer, 1, middleware);
-
-store.subscribe(() => {
-    console.log("store changed", store.getState());
+store.dispatch((dispatch) => {
+    dispatch({type: "FOO"});
+    // do something async
+    dispatch({type: "BAR"});
 });
-
-store.dispatch({ type: "INC" });
-store.dispatch({ type: "INC" });
-store.dispatch({ type: "DEC" });
-store.dispatch({ type: "DEC" });
-store.dispatch({ type: "ERR" });
